7f8e00b5f36fbbcf1173e6a91c8b42f17019ef4c
Habibullah Nahid <nahidex@gmail.com>
2015-12-02 04:07:08 +0600

    Initial Commit

---
+++ app/Task.php
@@ -0,0 +1,16 @@
+<?php
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $fillable = ['name'];
+    
+    /**
+     * Get the user that owns the task.
+     */
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+}
